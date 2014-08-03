f12d6a57591221f3b91d1e980065e1e7d8bf2729
Jeff Madsen <jrmadsen67@gmail.com>
2014-08-03 11:43:27 +0900

    simple master template

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">		
+	</head>
+    <body>
+
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
--- app/views/simple/get_all.blade.php
+++ app/views/simple/get_all.blade.php
@@ -1,7 +1,9 @@
+@extends('layouts.master')
 
-
+@section('content')
 <ul>
 	@foreach ($posts as $post)
 		<li>{{ $post->title}}</li>
 	@endforeach
 </ul>
+@stop
